a5dc913d856afcbd3edf48dd30671bef82ba5e48
maryam-choudhury <161529591+maryam-choudhury@users.noreply.github.com>
2024-09-11 11:43:18 -0400

    Add files via upload

---
+++ gettingStarted/helloWorld.cpp
@@ -0,0 +1,24 @@
+
+// This tells the program to use the iostream library
+// iostream lets us do things like print text on the screen
+#include <iostream>
+
+// This line saves us from having to type 'std::' before standard commands like 'cout'
+using namespace std;
+
+int main()  // Every C++ program starts from the main function
+{
+    /* 
+    cout is short for "character output" and it prints our insertions to the screen
+    Here, it's going to print "Hello World!" when we run the program
+    */
+    cout << "Hello World!";
+}
+
+// End of the main function
+
+//Throughout this source code we utilized single...
+ 
+/*
+...and multi-line comments.
+*/
